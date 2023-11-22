4bb332a142b4946a02d7529fef27f8bf658d7f94
ekwunifeifenna <ifennaekwunife73@gmail.com>
2023-11-21 21:34:40 -0500

    Modify the chatbot add a loading state error state and an empty state

--- src/components/AIChatBox.tsx
+++ src/components/AIChatBox.tsx
@@ -1,8 +1,12 @@
 import { cn } from "@/lib/utils";
 import { useChat } from "ai/react";
-import { XCircle } from "lucide-react";
+import { Bot, Trash, XCircle } from "lucide-react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { Message } from "ai";
+import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
+import { useEffect, useRef } from "react";
 
 interface AIChatBoxProps {
   open: boolean;
@@ -18,7 +22,23 @@ export default function AIChatBox({ open, onClose }: AIChatBoxProps) {
     setMessages,
     isLoading,
     error,
-  } = useChat();
+  } = useChat(); // /api/chat
+  const inputRef = useRef<HTMLInputElement>(null);
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+    }
+  }, [messages]);
+
+  useEffect(() => {
+    if (open) {
+      inputRef.current?.focus;
+    }
+  }, [open]);
+
+  const lastMessageIsUser = messages[messages.length - 1]?.role === "user";
 
   return (
     <div
@@ -31,16 +51,93 @@ export default function AIChatBox({ open, onClose }: AIChatBoxProps) {
         <XCircle size={30} />
       </button>
 
-      <div className="flex h-[600px] flex-col rounded bg-background border shadow-xl">
-        <div className="h-full">Messages</div>
+      <div className="flex h-[600px] flex-col rounded border bg-background shadow-xl">
+        <div className="mt-3 h-full overflow-y-auto px-3" ref={scrollRef}>
+          {messages.map((message) => (
+            <ChatMessage message={message} key={message.id} />
+          ))}
+          {isLoading && lastMessageIsUser && (
+            <ChatMessage
+              message={{
+                role: "assistant",
+                content: "one sec...",
+              }}
+            />
+          )}
+
+          {!error && messages.length === 0 && (
+            <div className="flex h-full items-center justify-center gap-3">
+              <Bot />
+              Ask the AI a question about your notes
+            </div>
+          )}
+
+          {!error && (
+            <ChatMessage
+              message={{
+                role: "assistant",
+                content: "something went wrong. Please try again",
+              }}
+            />
+          )}
+        </div>
         <form onSubmit={handleSubmit} className="m-3 flex gap-1">
-            <Input 
+          <Button
+            title="clear chat"
+            variant="outline"
+            size="icon"
+            className="shrink-0"
+            type="button"
+            onClick={() => setMessages([])}
+          >
+            <Trash />
+          </Button>
+          <Input
             value={input}
             onChange={handleInputChange}
-            placeholder="Say something..."/>
-            <Button type="submit">Send</Button>
+            placeholder="Say something..."
+            ref={inputRef}
+          />
+          <Button type="submit">Send</Button>
         </form>
       </div>
+    </div>
+  );
+}
+
+function ChatMessage({
+  message: { role, content },
+}: {
+  message: Pick<Message, "role" | "content">;
+}) {
+  const { user } = useUser();
+
+  const isAiMessage = role === "assistant";
+  return (
+    <div
+      className={cn(
+        "mb-3 flex items-center",
+        isAiMessage ? "me-5 justify-start" : "ms-5 justify-end",
+      )}
+    >
+      {isAiMessage && <Bot className="mr-2 shrink-0" />}
+      <p
+        className={cn(
+          "whitespace-pre-line rounded-md border px-3 py-2",
+          isAiMessage ? "bg-background" : "bg-primary text-primary-foreground",
+        )}
+      >
+        {content}
+      </p>
+      {!isAiMessage && user?.imageUrl && (
+        <Image
+          src={user.imageUrl}
+          alt="user Image"
+          width={100}
+          height={100}
+          className="ml-2 h-10 w-10 rounded-full object-cover"
+        />
+      )}
     </div>
   );
 }
